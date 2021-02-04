1bbfa9f2ef69923ffa86c34b41da9aed69235446
redebron20 <redebron20@gmail.com>
2021-02-04 03:57:46 -0500

    commit tree e9372100d0578a96ef63e723089d6e1eeaf28aa3

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
+
 class Pirate
+    attr_accessor :name, :weight, :height
+    @@pirates = []
+
+    def initialize(data)
+        @name = data[:name]
+        @weight = data[:weight]
+        @height = data[:height]
+        @@pirates << self
+    end
+
+    def self.all
+        @@pirates
+    end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+    attr_accessor :name, :type, :booty
+    @@ships = []
+
+    def initialize(data)
+        @name = data[:name]
+        @type = data[:type]
+        @booty = data[:booty]
+        @@ships << self
+    end
+
+
+    def self.all
+        @@ships
+    end
+
+    def self.clear
+        @@ships = []
+    end
+
 end
